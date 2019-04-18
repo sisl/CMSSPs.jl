0681d4dde09b7337620a208f83fc4045625ca7d5
Shushman <shushmanchoudhury@gmail.com>
2019-04-18 16:15:09 -0700

    Add scripts and params for DREAMR

---
+++ scripts/local_policies/dreamr/dreamr_cf_policy.jl
@@ -0,0 +1,57 @@
+using GridInterpolations
+using LocalFunctionApproximation
+using LocalApproximationValueIteration
+using POMDPs
+using POMDPModels
+using POMDPModelTools
+using StaticArrays
+using JLD2, FileIO
+using Random
+using Logging
+using Distributions
+using CMSSPs
+
+# Read parameters from file
+# scale_file = ARGS[1]
+# simtime_file = ARGS[2]
+# cost_file = ARGS[3]
+# policy_name = ARGS[4]
+# poly_or_exp = ARGS[5]
+
+scale_file = "../../paramsets/dreamr/scale-small-test.toml"
+simtime_file = "../../paramsets/dreamr/simtime-small-test.toml"
+cost_file = "../../paramsets/dreamr/cost-1.toml"
+policy_name = "dreamr-cf-smalltest"
+poly_or_exp = "poly"
+
+rng = MersenneTwister(10)
+
+params = parse_params(scale_file=scale_file, simtime_file=simtime_file, cost_file=cost_file)
+
+cf_mdp = get_cf_mdp(MultiRotorUAVState, MultiRotorUAVAction, params, 0.75)
+
+if poly_or_exp == "poly"
+
+    xy_spacing = polyspace_symmetric(params.scale_params.XY_LIM, params.scale_params.XY_AXISVALS)
+    xydot_spacing = polyspace_symmetric(params.scale_params.XYDOT_LIM, params.scale_params.XYDOT_AXISVALS)
+
+elseif poly_or_exp == "exp"
+    
+    xy_spacing = log2space_symmetric(params.scale_params.XY_LIM, params.scale_params.XY_AXISVALS)
+    xydot_spacing = log2space_symmetric(params.scale_params.XYDOT_LIM, params.scale_params.XYDOT_AXISVALS)
+
+end
+
+state_grid = RectangleGrid(xy_spacing, xy_spacing, xydot_spacing, xydot_spacing)
+@show length(vertices(state_grid))
+lfa = LocalGIFunctionApproximator(state_grid)
+
+compute_terminalcost_localapprox!(cf_mdp, lfa)
+
+cf_policy = finite_horizon_VI_localapprox!(cf_mdp, lfa, true, params.scale_params.MC_GENERATIVE_NUMSAMPLES, rng)
+
+compute_min_value_per_horizon_localapprox!(cf_policy)
+
+inhor_fn = string(policy_name, "-inhor.jld2")
+outhor_fn = string(policy_name, "-outhor.jld2")
+save_modal_horizon_policy_localapprox(cf_policy, inhor_fn,outhor_fn, cf_mdp)
---
+++ scripts/local_policies/dreamr/dreamr_uf_policy.jl
@@ -0,0 +1,54 @@
+using GridInterpolations
+using LocalFunctionApproximation
+using LocalApproximationValueIteration
+using POMDPs
+using POMDPModels
+using POMDPModelTools
+using StaticArrays
+using JLD2, FileIO
+using Random
+using Logging
+using Distributions
+using CMSSPs
+
+
+# scale_file = ARGS[1]
+# simtime_file = ARGS[2]
+# cost_file = ARGS[3]
+# policy_name = ARGS[4]
+# poly_or_exp = ARGS[5]
+
+scale_file = "../../paramsets/dreamr/scale-small-test.toml"
+simtime_file = "../../paramsets/dreamr/simtime-small-test.toml"
+cost_file = "../../paramsets/dreamr/cost-1.toml"
+policy_name = "dreamr-uf-smalltest"
+poly_or_exp = "poly"
+
+
+rng = MersenneTwister(10)
+
+params = parse_params(scale_file=scale_file, simtime_file=simtime_file, cost_file=cost_file)
+
+uf_mdp = get_uf_mdp(MultiRotorUAVState, MultiRotorUAVAction, params)
+
+if poly_or_exp == "poly"
+
+    xy_spacing = polyspace_symmetric(params.scale_params.XY_LIM, params.scale_params.XY_AXISVALS)
+    xydot_spacing = polyspace_symmetric(params.scale_params.XYDOT_LIM, params.scale_params.XYDOT_AXISVALS)
+
+elseif poly_or_exp == "exp"
+    
+    xy_spacing = log2space_symmetric(params.scale_params.XY_LIM, params.scale_params.XY_AXISVALS)
+    xydot_spacing = log2space_symmetric(params.scale_params.XYDOT_LIM, params.scale_params.XYDOT_AXISVALS)
+
+end
+
+state_grid = RectangleGrid(xy_spacing, xy_spacing, xydot_spacing, xydot_spacing)
+@show length(vertices(state_grid))
+
+lfa = LocalGIFunctionApproximator(state_grid)
+
+uf_policy = infinite_horizon_VI_localapprox(uf_mdp, lfa, 20, true, params.scale_params.MC_GENERATIVE_NUMSAMPLES, rng)
+
+policy_fn = string(policy_name,".jld2")
+save_localapproxvi_policy_to_jld2(policy_fn, uf_policy, uf_mdp)
